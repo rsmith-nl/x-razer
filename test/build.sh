e95c0c17a8e824b7226868fa83876affa37e3524
Roland Smith <rsmith@xs4all.nl>
2025-08-28 11:39:24 +0200

    Read serial and formware version.

---
+++ test/build.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+cc -std=c11 -o razer-get-serial razer-get-serial.c -lusb
--- test/razer-get-serial.c
+++ test/razer-get-serial.c
@@ -5,7 +5,7 @@
 // Author: R.F. Smith <rsmith@xs4all.nl>
 // SPDX-License-Identifier: Unlicense
 // Created: 2025-08-27 22:43:50 +0200
-// Last modified: 2025-08-28T00:54:39+0200
+// Last modified: 2025-08-28T11:08:20+0200
 
 // Compile with “cc -std=c11 -o razer-get-serial razer-get-serial.c -lusb”
 
@@ -30,10 +30,20 @@ typedef struct {
   uint8_t reserved; /*0x0*/
 } Razer_report;
 
+
+uint8_t calculate_crc(Razer_report *report) {
+  uint8_t *_report = (uint8_t*)report;
+  uint8_t crc = 0;
+  for (int j = 2; j < 88; j++) {
+    crc ^= _report[j];
+  }
+  return crc;
+}
+
 void print_serial(libusb_device_handle *handle){
   // First control message
   int bytes_transferred = 0;
-  Razer_report serial_message = {
+  Razer_report out_message = {
     .status = 0x00,
     .transaction_id = 0xff,
     .remaining_packets = 0x00,
@@ -42,14 +52,9 @@ void print_serial(libusb_device_handle *handle){
     .command_class = 0x00,
     .command_id = 0x82,
   };
-  uint8_t checksum = 0;
-  uint8_t *u8ptr = (uint8_t*)&serial_message.transaction_id;
-  for (int32_t k = 2; k < 88; k++) {
-    checksum ^= *u8ptr++;
-  }
-  serial_message.crc = checksum;
+  out_message.crc = calculate_crc(&out_message);
   bytes_transferred = libusb_control_transfer(handle, 0x21, 0x09, 0x300, 0x01,
-                      (uint8_t*)&serial_message, 90, 0);
+                      (uint8_t*)&out_message, 90, 0);
   if (bytes_transferred != 90) {
     //fprintf(stderr, "First control message failed.\n");
     return;
@@ -71,6 +76,41 @@ void print_serial(libusb_device_handle *handle){
   memcpy(sn, return_message.arguments, datasize);
   printf("Serial number: %s\n", sn);
 }
+
+void print_firmware(libusb_device_handle *handle){
+  // First control message
+  int bytes_transferred = 0;
+  Razer_report out_message = {
+    .status = 0x00,
+    .transaction_id = 0xff,
+    .remaining_packets = 0x00,
+    .protocol_type = 0x00,
+    .data_size = 0x02,
+    .command_class = 0x00,
+    .command_id = 0x81,
+  };
+  out_message.crc = calculate_crc(&out_message);
+  bytes_transferred = libusb_control_transfer(handle, 0x21, 0x09, 0x300, 0x01,
+                      (uint8_t*)&out_message, 90, 0);
+  if (bytes_transferred != 90) {
+    //fprintf(stderr, "First control message failed.\n");
+    return;
+  }
+  // Second control message
+  Razer_report return_message;
+  bytes_transferred = libusb_control_transfer(handle, 0xa1, 0x01, 0x300, 0x01,
+                      (uint8_t*)&return_message, 90, 0);
+  if (bytes_transferred != 90) {
+    //fprintf(stderr, "Second control message failed.\n");
+    return;
+  }
+  //fprintf(stderr, "DEBUG: status = %hhx\n", return_message.status);
+  printf("Firmware version: v%d.%d\n", return_message.arguments[0],
+      return_message.arguments[1]);
+}
+
+
+
 
 int main(int argc, char *argv[])
 {
@@ -109,6 +149,7 @@ int main(int argc, char *argv[])
       printf("Product: %s\n", buf);
     }
     print_serial(handle);
+    print_firmware(handle);
   }
 exit:
   libusb_free_device_list(device_list, 1);
